3673cf94d4105c8cedff50735e6f267433be95b8
sebanime02 <sebastianbell2@hotmail.com>
2014-11-17 00:52:15 -0500

    v1.0.1.1

---
+++ src/com/androidya/proyecto026/MainActivity.java
@@ -0,0 +1,98 @@
+package com.androidya.proyecto026;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.View;
+
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnCompletionListener {
+	TextView tv1;
+	MediaRecorder recorder;
+	MediaPlayer player;
+	File archivo;
+	ImageButton b1, b2, b3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		tv1 = (TextView) this.findViewById(R.id.textView1);
+		b1 = (ImageButton) findViewById(R.id.button1);
+		b2 = (ImageButton) findViewById(R.id.button2);
+		b3 = (ImageButton) findViewById(R.id.button3);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void grabar(View v) {
+		recorder = new MediaRecorder();
+		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		File path = new File(Environment.getExternalStorageDirectory()
+				.getPath());
+		try {
+			archivo = File.createTempFile("temporal", ".3gp", path);
+		} catch (IOException e) {
+		}
+		recorder.setOutputFile(archivo.getAbsolutePath());
+		try {
+			recorder.prepare();
+		} catch (IOException e) {
+		}
+		recorder.start();
+		tv1.setText("Grabando");
+		b1.setEnabled(false);
+		b2.setEnabled(true);
+	}
+
+	public void detener(View v) {
+		recorder.stop();
+		recorder.release();
+		player = new MediaPlayer();
+		player.setOnCompletionListener(this);
+		try {
+			player.setDataSource(archivo.getAbsolutePath());
+		} catch (IOException e) {
+		}
+		try {
+			player.prepare();
+		} catch (IOException e) {
+		}
+		b1.setEnabled(true);
+		b2.setEnabled(false);
+		b3.setEnabled(true);
+		tv1.setText("Listo para reproducir");
+	}
+
+	public void reproducir(View v) {
+		player.start();
+		b1.setEnabled(false);
+		b2.setEnabled(false);
+		b3.setEnabled(false);
+		tv1.setText("Reproduciendo");
+	}
+
+	public void onCompletion(MediaPlayer mp) {
+		b1.setEnabled(true);
+		b2.setEnabled(true);
+		b3.setEnabled(true);
+		tv1.setText("Listo");
+	}
+}
